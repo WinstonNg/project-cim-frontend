b33dfef4a1b096d981a9971698d885f4216855b6
WinstonNg <wincern@gmail.com>
2022-02-23 01:03:28 +0800

    feat: add customer page
    add customer page to render

---
+++ src/pages/customer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CustomersContainer from "../containers/Customers";
+
+const Customers = () => {
+  return (
+    <div>
+      <CustomersContainer title="Customers" breadcrumb="Customers" />
+    </div>
+  );
+};
+
+export default Customers;
